e443f2505c754af1fc3f0cfe9bda520bdf3ebfa8
Daniel <anndannma@outlook.com>
2024-05-02 13:53:28 -0500

    Agregué cambios

--- pages/components/GoogleMap.js
+++ pages/components/GoogleMap.js
@@ -51,7 +51,7 @@ function initMap() {
 
 export default function Map() {
   useEffect(() => {
-    loadGoogleMapsScript('AIzaSyBN-7bCv17Bf9_KO47_VitgN_Iqr28g7g4', initMap);
+    loadGoogleMapsScript('AIzaSyA3cMZAk_egY7CXoEFh_2XBvk-3NWs_uFU', initMap);
   }, []);
 
   return (
